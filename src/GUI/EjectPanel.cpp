bfbe77902100124cbec8e67cb658d30f4bd4ae3c
Tenshi <tenshii.tv@gmail.com>
2023-11-12 04:34:08 +0100

    Add separator for each mod

--- src/GUI/EjectPanel.cpp
+++ src/GUI/EjectPanel.cpp
@@ -5,6 +5,23 @@
 
 namespace Xash::GUI
 {
+    void Imgui::DrawModInfos(const Injector::ModInfos &loadedModInfos,
+        const std::pair<Injector::ModInfos, Injector::MonoModule> &loadedMod)
+    {
+        Injector::ModsManager &modsManager = Injector::ModsManager::getInstance();
+
+        ImGui::Text("Mod name: %s", loadedModInfos.modClass.c_str());
+        ImGui::Text("Mod Namespace: %s", loadedModInfos.modNamespace.c_str());
+        ImGui::Text("Mod Path: %s", loadedModInfos.modPath.c_str());
+        ImGui::Text("Targeted process: %s", loadedModInfos.targetedProcessName.c_str());
+		if (ImGui::Button("Eject"))
+		{
+			Injector::ModInfos newModInfos = loadedMod.first;
+			newModInfos.modUnloadMethod = mModInfos.modUnloadMethod;
+			modsManager.UnLoadMod(newModInfos);
+		}
+	}
+
 	void Imgui::DrawEjectPanel()
     {
         Injector::ModsManager &modsManager = Injector::ModsManager::getInstance();
@@ -20,20 +37,12 @@ namespace Xash::GUI
         strcpy_s(unloadMethodNameBuffer, mModInfos.modUnloadMethod.c_str());
         ImGui::InputText("Unload method", unloadMethodNameBuffer, sizeof(unloadMethodNameBuffer));
         mModInfos.modUnloadMethod = unloadMethodNameBuffer;
+        ImGui::Separator();
 
         for (auto &loadedMod : loadedMods)
         {
-            ImGui::Text("--------------------");
-            ImGui::Text("Mod name: %s", loadedMod.first.modClass.c_str());
-            ImGui::Text("Mod Namespace: %s", loadedMod.first.modNamespace.c_str());
-            ImGui::Text("Mod Path: %s", loadedMod.first.modPath.c_str());
-            ImGui::Text("Targeted process: %s", loadedMod.first.targetedProcessName.c_str());
-            if (ImGui::Button("Eject"))
-            {
-                Injector::ModInfos modInfos = loadedMod.first;
-                modInfos.modUnloadMethod = mModInfos.modUnloadMethod;
-                modsManager.UnLoadMod(modInfos);
-            }
+            DrawModInfos(loadedMod.first, loadedMod);
+            ImGui::Separator();
         }
 	}
 }
