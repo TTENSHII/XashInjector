90d95349fd7dabf0cf7314925c762620eed7f05a
Tenshi <tenshii.tv@gmail.com>
2023-11-12 04:34:08 +0100

    Add separator for each mod

--- src/GUI/Imgui.hpp
+++ src/GUI/Imgui.hpp
@@ -5,59 +5,68 @@
 #include <d3d11.h>
 #include <vector>
 #include <string>
+#include <utility>
 #include "ModInfos.hpp"
+#include "MonoModule.hpp"
 #include "imgui.h"
 
-namespace Xash::GUI
+namespace Xash
 {
-	class Imgui
+	namespace GUI
 	{
-		static constexpr ImVec2 mainWindowPos = ImVec2(120, 0);
-
-	public:
-		Imgui();
-		~Imgui();
-
-		void InitWin32AndDX11(
-			const HWND &window, Microsoft::WRL::ComPtr<ID3D11Device> device,
-			Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext
-		);
-		void Draw();
-		void Render();
-
-	private:
-		enum class ActivePanel : uint8_t
+		class Imgui
 		{
-			INJECT,
-			EJECT,
-			CONFIG,
-			SETTINGS
+			static constexpr ImVec2 mainWindowPos = ImVec2(120, 0);
+
+		public:
+			Imgui();
+			~Imgui();
+
+			void InitWin32AndDX11(
+				const HWND &window, Microsoft::WRL::ComPtr<ID3D11Device> device,
+				Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext
+			);
+			void Draw();
+			void Render();
+
+		private:
+			enum class ActivePanel : uint8_t
+			{
+				INJECT,
+				EJECT,
+				CONFIG,
+				SETTINGS
+			};
+
+			void DrawSideBar();
+			void DrawPanels();
+			void MaximizeMainWindow();
+
+			// INJECT
+			void DrawInjectPanel();
+			void DrawProcessesBox();
+			void DrawDllBox();
+			void DrawInjectInputs();
+			void DrawInjectButton();
+			void GetProcessesNames();
+
+			// EJECT
+			void DrawEjectPanel();
+			void DrawModInfos(
+				const Injector::ModInfos &modInfos,
+				const std::pair<Injector::ModInfos, Injector::MonoModule> &loadedMod
+			);
+
+			// CONFIG
+			void DrawConfigPanel();
+
+			// SETTINGS
+			void DrawSettingsPanel();
+
+			Injector::ModInfos mModInfos;
+			std::size_t mSelectedProcessIndex = 0;
+			std::vector<std::string> mProcessesNames;
+			ActivePanel mActivePanel = ActivePanel::INJECT;
 		};
-
-		void DrawSideBar();
-		void DrawPanels();
-		void MaximizeMainWindow();
-
-		// INJECT
-		void DrawInjectPanel();
-		void DrawProcessesBox();
-		void DrawDllBox();
-		void DrawInjectInputs();
-		void DrawInjectButton();
-		void GetProcessesNames();
-
-		// EJECT
-		void DrawEjectPanel();
-
-		// CONFIG
-		void DrawConfigPanel();
-
-		// SETTINGS
-		void DrawSettingsPanel();
-
-		Injector::ModInfos mModInfos;
-		std::size_t mSelectedProcessIndex = 0;
-		std::vector<std::string> mProcessesNames;
-		ActivePanel mActivePanel = ActivePanel::INJECT;
-	};
-} // namespace Xash::GUI
+	} // namespace GUI
+} // namespace Xash
